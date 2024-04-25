9979bfe219fa6ebb7e052d7414d00a117d6f7e78
tushardahiyaa <tushardahiya202@gmail.com>
2024-04-25 12:16:41 +0530

    final upload of tested propReturn

--- propreturns-nextjs/app/properties/components/FetchProperties.tsx
+++ propreturns-nextjs/app/properties/components/FetchProperties.tsx
@@ -292,6 +292,7 @@ export default function FetchProperties() {
                             alt="property Image"
                             placeholder="blur"
                             quality={40}
+                            priority
                           />
                           {property.verifiedStatus ? (
                             <div className="flex items-center gap-2 bg-sky-200 border-2 border-sky-500 px-2 py-2 rounded-full opacity-90 absolute top-3 left-4">
