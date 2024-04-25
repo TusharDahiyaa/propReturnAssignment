bda17de638d3c5e951aab3187be1917d9f199d97
tushardahiyaa <tushardahiya202@gmail.com>
2024-04-25 11:41:24 +0530

    final upload of tested propReturn

--- propreturns-nextjs/app/properties/components/FetchProperties.tsx
+++ propreturns-nextjs/app/properties/components/FetchProperties.tsx
@@ -291,7 +291,8 @@ export default function FetchProperties() {
                             className="w-full h-full object-cover rounded-t-xl md:rounded-none"
                             alt="property Image"
                             placeholder="blur"
-                            priority
+                            width={property.images[0].width}
+                            height={property.images[0].height}
                           />
                           {property.verifiedStatus ? (
                             <div className="flex items-center gap-2 bg-sky-200 border-2 border-sky-500 px-2 py-2 rounded-full opacity-90 absolute top-3 left-4">
