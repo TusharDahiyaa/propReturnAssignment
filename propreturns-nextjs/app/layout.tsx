ff279ee4b71dff2f11f482d1deaac62599e71e7e
tushardahiyaa <tushardahiya202@gmail.com>
2024-04-25 13:18:57 +0530

    final upload of tested propReturn

--- propreturns-nextjs/app/layout.tsx
+++ propreturns-nextjs/app/layout.tsx
@@ -1,35 +1,31 @@
 import "./globals.css";
 import Head from "next/head";
 
+<Head>
+  <link rel="preload" href="/Commercial-Office-Space-1.webp" as="image" />
+  <link
+    rel="preload"
+    href="/depositphotos_235840898-stock-photo-chinese-employees-work-wework-working.webp"
+    as="image"
+  />
+  <link
+    rel="preload"
+    href="/depositphotos_649045064-stock-photo-corner-stylish-open-space-office.webp"
+    as="image"
+  />
+  <link rel="preload" href="/gettyimages-1145667876-612x612.webp" as="image" />
+  <link
+    rel="preload"
+    href="/photo-1571624436279-b272aff752b5.webp"
+    as="image"
+  />
+</Head>;
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  <Head>
-    <link rel="preload" href="/Commercial-Office-Space-1.avif" as="image" />
-    <link
-      rel="preload"
-      href="/depositphotos_235840898-stock-photo-chinese-employees-work-wework-working.avif"
-      as="image"
-    />
-    <link
-      rel="preload"
-      href="/depositphotos_649045064-stock-photo-corner-stylish-open-space-office.avif"
-      as="image"
-    />
-    <link
-      rel="preload"
-      href="/gettyimages-1145667876-612x612.avif"
-      as="image"
-    />
-    <link
-      rel="preload"
-      href="/photo-1571624436279-b272aff752b5.avif"
-      as="image"
-    />
-  </Head>;
-
   return (
     <html lang="en">
       <body>{children}</body>
